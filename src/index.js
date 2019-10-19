a76a4c89f2fec13775a24cf4ae02497981e5a589
Maria Yudenko <mariya.yudenko@gmail.com>
2019-10-19 23:54:00 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let string = str,
+  res = 0,
+  flag = 0;
+if( string.length !== 0 && res === 0 ) {
+  let j = 0;
+  while ( j < bracketsConfig.length ) {
+    const subString = bracketsConfig[j][0] + bracketsConfig[j][1];
+      if ( string.indexOf(bracketsConfig[j][0] + bracketsConfig[j][1]) !== -1 ) {
+        string = string.replace(subString, '');
+        flag += 1; 
+        j++;
+      }
+    if ( flag !== 0 ) { 
+      j = 0; 
+      flag = 0;
+    }
+    else { 
+      j++; 
+    }
+  }
+  
+if ( flag === 0 ) {
+  res = 1;
+}
+}
+
+return string === "";
+
 }
